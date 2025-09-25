cca01caa9846828e1ba3dfb68cedec101125e304
Faith Ruto <faith.ruto@ashesi.edu.gh>
2025-09-25 13:35:49 +0000

    Updated core.php with is_logged_in() and is_admin() functions

--- settings/core.php
+++ settings/core.php
@@ -7,9 +7,12 @@ session_start();
 ob_start();
 
 //funtion to check for login
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../login/login.php");
-    exit;
+function check_login()
+{
+    if (!isset($_SESSION['user_id'])) {
+        header("Location: ../login/login.php");
+        exit();
+    }
 }
 
 
